6daf45dd433542785b7bb93dc2f06bf037ecceda
JadKHaddad <jadkhaddad@gmail.com>
2024-09-10 19:31:49 +0200

    test(FramedRead): added tests for FrameSizeAwareDecoder
    Signed-off-by: JadKHaddad <jadkhaddad@gmail.com>

--- src/decode/framed_read.rs
+++ src/decode/framed_read.rs
@@ -146,7 +146,7 @@ const _: () = {
     use core::{
         borrow::BorrowMut,
         pin::{pin, Pin},
-        task::{ready, Context, Poll},
+        task::{Context, Poll},
     };
 
     use futures::{Future, Stream};
@@ -420,9 +420,9 @@ const _: () = {
                 // If we can't build a frame yet, try to read more data and try again.
 
                 let fut = pin!(this.inner.read(&mut state.buffer[state.index..]));
-                match ready!(fut.poll(cx)) {
+                match fut.poll(cx) {
                     // Pending -> implicit reading -> reading or implicit paused -> paused
-                    Err(err) => {
+                    Poll::Ready(Err(err)) => {
                         #[cfg(all(feature = "logging", feature = "tracing"))]
                         {
                             tracing::warn!("Failed to read");
@@ -433,7 +433,7 @@ const _: () = {
 
                         return Poll::Ready(Some(Err(Error::IO(err))));
                     }
-                    Ok(0) => {
+                    Poll::Ready(Ok(0)) => {
                         #[cfg(all(feature = "logging", feature = "tracing"))]
                         tracing::debug!("Got EOF");
 
@@ -454,7 +454,7 @@ const _: () = {
                         // prepare reading -> paused
                         state.eof = true;
                     }
-                    Ok(n) => {
+                    Poll::Ready(Ok(n)) => {
                         state.index += n;
 
                         #[cfg(all(feature = "logging", feature = "tracing"))]
@@ -463,11 +463,19 @@ const _: () = {
                         // prepare paused -> framing or noop reading -> framing
                         state.eof = false;
                     }
+                    Poll::Pending => {
+                        #[cfg(all(feature = "logging", feature = "tracing"))]
+                        tracing::trace!("Pending");
+
+                        return Poll::Pending;
+                    }
                 }
 
                 match state.frame_size {
                     Some(frame_size) => {
-                        if state.index - state.total_consumed >= frame_size {
+                        let frame_size_reached = state.index - state.total_consumed >= frame_size;
+
+                        if frame_size_reached {
                             #[cfg(all(feature = "logging", feature = "tracing"))]
                             {
                                 tracing::trace!(frame_size, "Frame size reached");
@@ -478,6 +486,11 @@ const _: () = {
                             state.is_framable = true;
 
                             state.frame_size = None;
+                        }
+
+                        #[cfg(all(feature = "logging", feature = "tracing"))]
+                        if !frame_size_reached {
+                            tracing::trace!(frame_size, index=%state.index, "Frame size not reached");
                         }
                     }
                     None => {
@@ -490,17 +503,25 @@ const _: () = {
     }
 };
 
-#[cfg(test)]
+#[cfg(all(test, feature = "tokio"))]
 mod test {
     extern crate std;
 
     use std::vec::Vec;
 
     use futures::StreamExt;
+    use tokio::io::AsyncWriteExt;
+
+    #[cfg(all(
+        feature = "logging",
+        any(feature = "log", feature = "defmt", feature = "tracing")
+    ))]
+    use crate::logging::formatter::Formatter;
 
     use crate::{
         decode::{decoder::Decoder, frame::Frame},
         test::init_tracing,
+        tokio::Compat,
     };
 
     use super::*;
@@ -573,6 +594,134 @@ mod test {
 
         let last_item = items.last().expect("No items");
         assert!(matches!(last_item, Err(Error::BadDecoder)));
+    }
+
+    struct FrameSizeAwareDecoder;
+
+    impl Decoder for FrameSizeAwareDecoder {
+        type Item = ();
+        type Error = ();
+
+        fn decode(&mut self, src: &mut [u8]) -> Result<MaybeDecoded<Self::Item>, Self::Error> {
+            #[cfg(all(feature = "logging", feature = "tracing"))]
+            {
+                let src = Formatter(src);
+                tracing::debug!(?src, "Decoding");
+            }
+
+            if src.len() < 4 {
+                #[cfg(all(feature = "logging", feature = "tracing"))]
+                tracing::debug!("Not enough bytes to read frame size");
+
+                return Ok(MaybeDecoded::None(FrameSize::Unknown));
+            }
+
+            let size = u32::from_be_bytes([src[0], src[1], src[2], src[3]]) as usize;
+
+            #[cfg(all(feature = "logging", feature = "tracing"))]
+            tracing::debug!(size, "Frame size");
+
+            if src.len() < size {
+                #[cfg(all(feature = "logging", feature = "tracing"))]
+                tracing::debug!("Not enough bytes to read frame");
+
+                return Ok(MaybeDecoded::None(FrameSize::Known(size)));
+            }
+
+            Ok(MaybeDecoded::Frame(Frame::new(size, ())))
+        }
+    }
+
+    fn generate_chunks() -> Vec<Vec<u8>> {
+        std::vec![
+            Vec::from(b"\x00\x00\x00\x0f"),
+            Vec::from(b"hello world"),
+            Vec::from(b"\x00\x00\x00\x0f"),
+            Vec::from(b"hello world\x00\x00\x00\x0f"),
+            Vec::from(b"hello world"),
+            Vec::from(b"\x00\x00"),
+            Vec::from(b"\x00\x0fhello"),
+            Vec::from(b" world"),
+            Vec::from(b"\x00\x00\x00\x0fhello world\x00\x00\x00"),
+            Vec::from(b"\x0f"),
+            Vec::from(b"hell"),
+            Vec::from(b"o wo"),
+            Vec::from(b"rld"),
+            Vec::from(b"\x00\x00\x00\x0f"),
+            Vec::from(b"h"),
+            Vec::from(b"e"),
+            Vec::from(b"l"),
+            Vec::from(b"l"),
+            Vec::from(b"o"),
+            Vec::from(b" "),
+            Vec::from(b"w"),
+            Vec::from(b"o"),
+            Vec::from(b"r"),
+            Vec::from(b"l"),
+            Vec::from(b"d\x00\x00\x00\x0f"),
+            Vec::from(b"hello world"),
+            Vec::from(b"\x00\x00\x00\x0f"),
+            Vec::from(b"hello world"),
+        ]
+    }
+
+    async fn decode_with_frame_size<const I: usize>(
+        byte_chunks: Vec<Vec<u8>>,
+    ) -> Vec<Result<(), Error<std::io::Error, ()>>> {
+        let (read, mut write) = tokio::io::duplex(1024);
+
+        tokio::spawn(async move {
+            for chunk in byte_chunks {
+                write.write_all(&chunk).await.unwrap();
+                tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+            }
+        });
+
+        let read = Compat::new(read);
+
+        let codec = FrameSizeAwareDecoder;
+        let buf = &mut [0_u8; I];
+
+        let framed_read = FramedRead::new(read, codec, buf);
+        let byte_chunks: Vec<_> = framed_read.collect().await;
+
+        byte_chunks.into_iter().collect::<Vec<_>>()
+    }
+
+    #[tokio::test]
+    async fn decode_with_frame_size_buffer_64() {
+        init_tracing();
+
+        let items = decode_with_frame_size::<64>(generate_chunks()).await;
+
+        assert!(items.iter().all(Result::is_ok));
+    }
+
+    #[tokio::test]
+    async fn decode_with_frame_size_buffer_32() {
+        init_tracing();
+
+        let items = decode_with_frame_size::<32>(generate_chunks()).await;
+
+        assert!(items.iter().all(Result::is_ok));
+    }
+
+    #[tokio::test]
+    async fn decode_with_frame_size_buffer_16() {
+        init_tracing();
+
+        let items = decode_with_frame_size::<16>(generate_chunks()).await;
+
+        assert!(items.iter().all(Result::is_ok));
+    }
+
+    #[tokio::test]
+    async fn decode_with_frame_size_buffer_8() {
+        init_tracing();
+
+        let items = decode_with_frame_size::<8>(generate_chunks()).await;
+
+        assert!(matches!(items.last(), Some(Err(Error::BufferTooSmall))));
     }
 
     // TODO test bytes remaining on stream
