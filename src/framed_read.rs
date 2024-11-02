c90b417f997c77ce254d39f271ec62902c0e38ee
Jad K. Haddad <jadkhaddad@gmail.com>
2024-11-02 17:19:50 +0100

    refactor(BincodeCodec): moved BincodeMessage to test module
    Signed-off-by: Jad K. Haddad <jadkhaddad@gmail.com>

--- src/framed_read.rs
+++ src/framed_read.rs
@@ -864,61 +864,25 @@ mod test {
     }
 
     #[tokio::test]
+    #[cfg(feature = "bincode")]
     async fn stream_sink_bincode() {
+        use crate::test::bincode::BincodeMessage;
         use bincode::serde::Compat as BincodeSerdeCompat;
-
-        #[derive(bincode::Encode, bincode::Decode)]
-        enum Message {
-            Numbers(u32, u32, u32),
-            String(BincodeSerdeCompat<heapless::String<32>>),
-            Vec(BincodeSerdeCompat<heapless::Vec<u8, 32>>),
-        }
-
-        impl core::fmt::Debug for Message {
-            fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-                match self {
-                    Self::Numbers(a, b, c) => write!(f, "Numbers({}, {}, {})", a, b, c),
-                    Self::String(s) => write!(f, "String({})", s.0),
-                    Self::Vec(v) => write!(f, "Vec({:?})", v.0),
-                }
-            }
-        }
-
-        impl Clone for Message {
-            fn clone(&self) -> Self {
-                match self {
-                    Self::Numbers(a, b, c) => Self::Numbers(*a, *b, *c),
-                    Self::String(s) => Self::String(BincodeSerdeCompat(s.0.clone())),
-                    Self::Vec(v) => Self::Vec(BincodeSerdeCompat(v.0.clone())),
-                }
-            }
-        }
-
-        impl PartialEq for Message {
-            fn eq(&self, other: &Self) -> bool {
-                match (self, other) {
-                    (Self::Numbers(a, b, c), Self::Numbers(x, y, z)) => a == x && b == y && c == z,
-                    (Self::String(s), Self::String(t)) => s.0 == t.0,
-                    (Self::Vec(v), Self::Vec(w)) => v.0 == w.0,
-                    _ => false,
-                }
-            }
-        }
 
         init_tracing();
 
-        let items: Vec<Message> = std::vec![
-            Message::Numbers(1, 2, 3),
-            Message::String(BincodeSerdeCompat(
+        let items: Vec<BincodeMessage> = std::vec![
+            BincodeMessage::Numbers(1, 2, 3),
+            BincodeMessage::String(BincodeSerdeCompat(
                 heapless::String::from_str("Hello").unwrap()
             )),
-            Message::Vec(BincodeSerdeCompat(
+            BincodeMessage::Vec(BincodeSerdeCompat(
                 heapless::Vec::from_slice(b"Hello, world!").unwrap()
             )),
         ];
 
-        let decoder = BincodeCodecOwned::<Message>::new();
-        let encoder = BincodeCodecOwned::<Message>::new();
+        let decoder = BincodeCodecOwned::<BincodeMessage>::new();
+        let encoder = BincodeCodecOwned::<BincodeMessage>::new();
 
         stream_sink!(encoder, decoder, items);
     }
--- src/test.rs
+++ src/test.rs
@@ -6,3 +6,46 @@ pub fn init_tracing() {
     )
     .ok();
 }
+
+#[cfg(feature = "bincode")]
+pub mod bincode {
+    use bincode::serde::Compat as BincodeSerdeCompat;
+
+    #[derive(bincode::Encode, bincode::Decode)]
+    pub enum BincodeMessage {
+        Numbers(u32, u32, u32),
+        String(BincodeSerdeCompat<heapless::String<32>>),
+        Vec(BincodeSerdeCompat<heapless::Vec<u8, 32>>),
+    }
+
+    impl core::fmt::Debug for BincodeMessage {
+        fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+            match self {
+                Self::Numbers(a, b, c) => write!(f, "Numbers({}, {}, {})", a, b, c),
+                Self::String(s) => write!(f, "String({})", s.0),
+                Self::Vec(v) => write!(f, "Vec({:?})", v.0),
+            }
+        }
+    }
+
+    impl Clone for BincodeMessage {
+        fn clone(&self) -> Self {
+            match self {
+                Self::Numbers(a, b, c) => Self::Numbers(*a, *b, *c),
+                Self::String(s) => Self::String(BincodeSerdeCompat(s.0.clone())),
+                Self::Vec(v) => Self::Vec(BincodeSerdeCompat(v.0.clone())),
+            }
+        }
+    }
+
+    impl PartialEq for BincodeMessage {
+        fn eq(&self, other: &Self) -> bool {
+            match (self, other) {
+                (Self::Numbers(a, b, c), Self::Numbers(x, y, z)) => a == x && b == y && c == z,
+                (Self::String(s), Self::String(t)) => s.0 == t.0,
+                (Self::Vec(v), Self::Vec(w)) => v.0 == w.0,
+                _ => false,
+            }
+        }
+    }
+}
