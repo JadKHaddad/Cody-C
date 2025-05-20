886bbd7646137e7bbed1dba92bb7004a96dd1bf8
Jad K. Haddad <jadkhaddad@gmail.com>
2025-05-20 07:57:30 +0200

    feat(bincode)!: removed bincode codec
    Signed-off-by: Jad K. Haddad <jadkhaddad@gmail.com>

--- src/codec/mod.rs
+++ src/codec/mod.rs
@@ -6,10 +6,3 @@ pub mod length;
 pub mod lines;
 
 pub use self::{any::*, bytes::*, length::*, lines::*};
-
-#[cfg(feature = "bincode")]
-#[cfg_attr(docsrs, doc(cfg(feature = "bincode")))]
-pub mod bincode;
-
-#[cfg(feature = "bincode")]
-pub use self::bincode::*;
